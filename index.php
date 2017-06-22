f41700400f162af045dd30de4b098f54c6af95ca
vivek gupta <vivek@coloredcow.com>
2017-06-22 12:37:52 +0530

    some ui changes

--- index.php
+++ index.php
@@ -29,52 +29,54 @@
         </nav> 
         
         <div class="container-fluid">
-            <br/>
-                <div class="container">
-                    <h2 id="party">ADD EVENT</h2>
-                    <form  method="post" id="event_form">          
-                        <div class="form-group row">
-                                <label for="theme_name" class="col-8 col-form-label ">Theme Name</label>
-                                    <div class="col-12">
-                                        <input type="text" class="form-control" id="event_name" name="event_name" placeholder="Theme Name">
-                                    </div>
-                                <label for="date" class="col-8 col-form-label">Date</label>
-                                    <div class="col-12">
-                                        <input type="date" class="form-control" name="event_date" id="event_date">
-                                    </div>
-                                <label for="venue" class="col-8 col-form-label">Venue</label>
-                                <div class="col-12">
-                                    <input type="text" class="form-control" id="event_venue" name="event_venue" placeholder="Venue">
+            <br/><center>
+                <div class="container form-group row">
+                    <div class="col-6 bg-faded">
+                        <h2 id="party" class="text-secondary">ADD EVENT</h2>
+                            <form  method="post" id="event_form">          
+                                <div class="form-group row">
+                                    <label for="theme_name" class="col-6 col-form-label ">Theme Name</label>
+                                        <div class="col-12 col-xs-4 col-sm-6 col-md-4">
+                                            <input type="text" class="form-control" id="event_name" name="event_name" placeholder="Theme Name">
+                                        </div>
+                                    <label for="date" class="col-6 col-form-label">Date</label>
+                                        <div class="col-12 col-xs-4 col-sm-6 col-md-4">
+                                            <input type="date" class="form-control" name="event_date" id="event_date">
+                                        </div>
+                                    <label for="venue" class="col-6 col-form-label">Venue</label>
+                                        <div class="col-12 col-xs-4 col-sm-6 col-md-4">
+                                            <input type="text" class="form-control" id="event_venue" name="event_venue" placeholder="Venue">
+                                        </div>
                                 </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="select" value="selectevent" id="addingevent">Submit </button>
-                    </form>
-                </div>
-                <br/><br/ >
-                <div class="container">
-                    <h2 id="addguest">GUESTS</h2>
-                    <form method="post" id="guest_form">     
-                        <div class="form-group row">
-                            <label for="Email" class="col-8 col-form-label">Email address</label>
-                            <div class="col-12">
-                                <input type="email" class="form-control" id="email_guest" placeholder="Enter email" name="email_guest">
-                            </div>
-                            <label for="guestname" class="col-8 col-form-label">Name Of The Guest</label>
-                            <div class="col-12">
-                               <input type="text" class="form-control required" id="guest_name" placeholder="Guest Name" name="guest_name">
-                            </div>
-                            <label for="contact_number" class="col-8 col-form-label">Phone No.</label>
-                            <div class="col-12">
-                               <input type="number" class="form-control required" id="guest_contact_number" placeholder="Contact Number" name="guest_contact_number">
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="select" value="selectguest" id="addition">Submit</button>
-                    </form>
-                    <br> 
-                    <div class="navbar navbar-brand">
-                           <span id="show_msg"></span>
+                                <button type="submit" class="btn btn-primary" name="select" value="selectevent" id="addingevent">Add Event</button>
+                            </form>
                     </div>
-                </div>
+                        <br/><br/ >
+                    <div class="col-6 bg-faded">
+                        <h2 id="addguest" class="text-secondary">ADD GUESTS</h2>
+                        <form method="post" id="guest_form" >     
+                            <div class="form-group row">
+                                <label for="Email" class="col-6 col-form-label">Email address</label>
+                                    <div class="col-12 col-xs-6 col-sm-6 col-md-4">
+                                        <input type="email" class="form-control" id="email_guest" placeholder="Enter email" name="email_guest">
+                                    </div>
+                                <label for="guestname" class="col-6 col-form-label">Name Of The Guest</label>
+                                    <div class="col-12 col-xs-6 col-sm-6 col-md-4">
+                                       <input type="text" class="form-control required" id="guest_name" placeholder="Guest Name" name="guest_name">
+                                    </div>
+                                <label for="contact_number" class="col-6 col-form-label">Phone No.</label>
+                                    <div class="col-12 col-xs-6 col-sm-6 col-md-4">
+                                       <input type="number" class="form-control required" id="guest_contact_number" placeholder="Contact Number" name="guest_contact_number">
+                                    </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="select" value="selectguest" id="addition">Add Guest</button>
+                        </form>
+                        <br> <!-- 
+                        <div class="navbar navbar-brand">
+                               <span id="show_msg"></span>
+                        </div> -->
+                    </div>
+                </div></center>
         </div>  
 
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
